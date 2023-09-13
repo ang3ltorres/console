62b4160202c4b4e23a276d4ec047f2babce7bbe0
Angel <angel.storres@alumnos.udg.mx>
2023-09-12 21:07:21 -0600

    color fix

--- include/console.hpp
+++ include/console.hpp
@@ -4,163 +4,156 @@
 
 namespace console
 {
-	struct Color
+	enum class color : uint32_t
 	{
-		uint8_t r;
-		uint8_t g;
-		uint8_t b;
+		alice_blue = 0xF0F8FF,
+		antique_white = 0xFAEBD7,
+		aqua = 0x00FFFF,
+		aquamarine = 0x7FFFD4,
+		azure = 0xF0FFFF,
+		beige = 0xF5F5DC,
+		bisque = 0xFFE4C4,
+		black = 0x000000,
+		blanched_almond = 0xFFEBCD,
+		blue = 0x0000FF,
+		blue_violet = 0x8A2BE2,
+		brown = 0xA52A2A,
+		burly_wood = 0xDEB887,
+		cadet_blue = 0x5F9EA0,
+		chartreuse = 0x7FFF00,
+		chocolate = 0xD2691E,
+		coral = 0xFF7F50,
+		cornflower_blue = 0x6495ED,
+		cornsilk = 0xFFF8DC,
+		crimson = 0xDC143C,
+		cyan = 0x00FFFF,
+		dark_blue = 0x00008B,
+		dark_cyan = 0x008B8B,
+		dark_golden_rod = 0xB8860B,
+		dark_gray = 0xA9A9A9,
+		dark_green = 0x006400,
+		dark_khaki = 0xBDB76B,
+		dark_magenta = 0x8B008B,
+		dark_olive_green = 0x556B2F,
+		dark_orange = 0xFF8C00,
+		dark_orchid = 0x9932CC,
+		dark_red = 0x8B0000,
+		dark_salmon = 0xE9967A,
+		dark_sea_green = 0x8FBC8F,
+		dark_slate_blue = 0x483D8B,
+		dark_slate_gray = 0x2F4F4F,
+		dark_turquoise = 0x00CED1,
+		dark_violet = 0x9400D3,
+		deep_pink = 0xFF1493,
+		deep_sky_blue = 0x00BFFF,
+		dim_gray = 0x696969,
+		dodger_blue = 0x1E90FF,
+		fire_brick = 0xB22222,
+		floral_white = 0xFFFAF0,
+		forest_green = 0x228B22,
+		fuchsia = 0xFF00FF,
+		gainsboro = 0xDCDCDC,
+		ghost_white = 0xF8F8FF,
+		gold = 0xFFD700,
+		golden_rod = 0xDAA520,
+		gray = 0x808080,
+		green = 0x008000,
+		green_yellow = 0xADFF2F,
+		honey_dew = 0xF0FFF0,
+		hot_pink = 0xFF69B4,
+		indian_red = 0xCD5C5C,
+		indigo = 0x4B0082,
+		ivory = 0xFFFFF0,
+		khaki = 0xF0E68C,
+		lavender = 0xE6E6FA,
+		lavender_blush = 0xFFF0F5,
+		lawn_green = 0x7CFC00,
+		lemon_chiffon = 0xFFFACD,
+		light_blue = 0xADD8E6,
+		light_coral = 0xF08080,
+		light_cyan = 0xE0FFFF,
+		light_golden_rod_yellow = 0xFAFAD2,
+		light_gray = 0xD3D3D3,
+		light_green = 0x90EE90,
+		light_pink = 0xFFB6C1,
+		light_salmon = 0xFFA07A,
+		light_sea_green = 0x20B2AA,
+		light_sky_blue = 0x87CEFA,
+		light_slate_gray = 0x778899,
+		light_steel_blue = 0xB0C4DE,
+		light_yellow = 0xFFFFE0,
+		lime = 0x00FF00,
+		lime_green = 0x32CD32,
+		linen = 0xFAF0E6,
+		magenta = 0xFF00FF,
+		maroon = 0x800000,
+		medium_aquamarine = 0x66CDAA,
+		medium_blue = 0x0000CD,
+		medium_orchid = 0xBA55D3,
+		medium_purple = 0x9370DB,
+		medium_sea_green = 0x3CB371,
+		medium_slate_blue = 0x7B68EE,
+		medium_spring_green = 0x00FA9A,
+		medium_turquoise = 0x48D1CC,
+		medium_violet_red = 0xC71585,
+		midnight_blue = 0x191970,
+		mint_cream = 0xF5FFFA,
+		misty_rose = 0xFFE4E1,
+		moccasin = 0xFFE4B5,
+		navajo_white = 0xFFDEAD,
+		navy = 0x000080,
+		old_lace = 0xFDF5E6,
+		olive = 0x808000,
+		olive_drab = 0x6B8E23,
+		orange = 0xFFA500,
+		orange_red = 0xFF4500,
+		orchid = 0xDA70D6,
+		pale_golden_rod = 0xEEE8AA,
+		pale_green = 0x98FB98,
+		pale_turquoise = 0xAFEEEE,
+		pale_violet_red = 0xDB7093,
+		papaya_whip = 0xFFEFD5,
+		peach_puff = 0xFFDAB9,
+		peru = 0xCD853F,
+		pink = 0xFFC0CB,
+		plum = 0xDDA0DD,
+		powder_blue = 0xB0E0E6,
+		purple = 0x800080,
+		rebecca_purple = 0x663399,
+		red = 0xFF0000,
+		rosy_brown = 0xBC8F8F,
+		royal_blue = 0x4169E1,
+		saddle_brown = 0x8B4513,
+		salmon = 0xFA8072,
+		sandy_brown = 0xF4A460,
+		sea_green = 0x2E8B57,
+		sea_shell = 0xFFF5EE,
+		sienna = 0xA0522D,
+		silver = 0xC0C0C0,
+		sky_blue = 0x87CEEB,
+		slate_blue = 0x6A5ACD,
+		slate_gray = 0x708090,
+		snow = 0xFFFAFA,
+		spring_green = 0x00FF7F,
+		steel_blue = 0x4682B4,
+		tan = 0xD2B48C,
+		teal = 0x008080,
+		thistle = 0xD8BFD8,
+		tomato = 0xFF6347,
+		turquoise = 0x40E0D0,
+		violet = 0xEE82EE,
+		wheat = 0xF5DEB3,
+		white = 0xFFFFFF,
+		white_smoke = 0xF5F5F5,
+		yellow = 0xFFFF00,
+		yellow_green = 0x9ACD32,
 	};
-
-	namespace color
-	{
-		extern const Color alice_blue;
-		extern const Color antique_white;
-		extern const Color aqua;
-		extern const Color aquamarine;
-		extern const Color azure;
-		extern const Color beige;
-		extern const Color bisque;
-		extern const Color black;
-		extern const Color blanched_almond;
-		extern const Color blue;
-		extern const Color blue_violet;
-		extern const Color brown;
-		extern const Color burly_wood;
-		extern const Color cadet_blue;
-		extern const Color chartreuse;
-		extern const Color chocolate;
-		extern const Color coral;
-		extern const Color cornflower_blue;
-		extern const Color cornsilk;
-		extern const Color crimson;
-		extern const Color cyan;
-		extern const Color dark_blue;
-		extern const Color dark_cyan;
-		extern const Color dark_golden_rod;
-		extern const Color dark_gray;
-		extern const Color dark_green;
-		extern const Color dark_khaki;
-		extern const Color dark_magenta;
-		extern const Color dark_olive_green;
-		extern const Color dark_orange;
-		extern const Color dark_orchid;
-		extern const Color dark_red;
-		extern const Color dark_salmon;
-		extern const Color dark_sea_green;
-		extern const Color dark_slate_blue;
-		extern const Color dark_slate_gray;
-		extern const Color dark_turquoise;
-		extern const Color dark_violet;
-		extern const Color deep_pink;
-		extern const Color deep_sky_blue;
-		extern const Color dim_gray;
-		extern const Color dodger_blue;
-		extern const Color fire_brick;
-		extern const Color floral_white;
-		extern const Color forest_green;
-		extern const Color fuchsia;
-		extern const Color gainsboro;
-		extern const Color ghost_white;
-		extern const Color gold;
-		extern const Color golden_rod;
-		extern const Color gray;
-		extern const Color green;
-		extern const Color green_yellow;
-		extern const Color honey_dew;
-		extern const Color hot_pink;
-		extern const Color indian_red;
-		extern const Color indigo;
-		extern const Color ivory;
-		extern const Color khaki;
-		extern const Color lavender;
-		extern const Color lavender_blush;
-		extern const Color lawn_green;
-		extern const Color lemon_chiffon;
-		extern const Color light_blue;
-		extern const Color light_coral;
-		extern const Color light_cyan;
-		extern const Color light_golden_rod_yellow;
-		extern const Color light_gray;
-		extern const Color light_green;
-		extern const Color light_pink;
-		extern const Color light_salmon;
-		extern const Color light_sea_green;
-		extern const Color light_sky_blue;
-		extern const Color light_slate_gray;
-		extern const Color light_steel_blue;
-		extern const Color light_yellow;
-		extern const Color lime;
-		extern const Color lime_green;
-		extern const Color linen;
-		extern const Color magenta;
-		extern const Color maroon;
-		extern const Color medium_aquamarine;
-		extern const Color medium_blue;
-		extern const Color medium_orchid;
-		extern const Color medium_purple;
-		extern const Color medium_sea_green;
-		extern const Color medium_slate_blue;
-		extern const Color medium_spring_green;
-		extern const Color medium_turquoise;
-		extern const Color medium_violet_red;
-		extern const Color midnight_blue;
-		extern const Color mint_cream;
-		extern const Color misty_rose;
-		extern const Color moccasin;
-		extern const Color navajo_white;
-		extern const Color navy;
-		extern const Color old_lace;
-		extern const Color olive;
-		extern const Color olive_drab;
-		extern const Color orange;
-		extern const Color orange_red;
-		extern const Color orchid;
-		extern const Color pale_golden_rod;
-		extern const Color pale_green;
-		extern const Color pale_turquoise;
-		extern const Color pale_violet_red;
-		extern const Color papaya_whip;
-		extern const Color peach_puff;
-		extern const Color peru;
-		extern const Color pink;
-		extern const Color plum;
-		extern const Color powder_blue;
-		extern const Color purple;
-		extern const Color rebecca_purple;
-		extern const Color red;
-		extern const Color rosy_brown;
-		extern const Color royal_blue;
-		extern const Color saddle_brown;
-		extern const Color salmon;
-		extern const Color sandy_brown;
-		extern const Color sea_green;
-		extern const Color sea_shell;
-		extern const Color sienna;
-		extern const Color silver;
-		extern const Color sky_blue;
-		extern const Color slate_blue;
-		extern const Color slate_gray;
-		extern const Color snow;
-		extern const Color spring_green;
-		extern const Color steel_blue;
-		extern const Color tan;
-		extern const Color teal;
-		extern const Color thistle;
-		extern const Color tomato;
-		extern const Color turquoise;
-		extern const Color violet;
-		extern const Color wheat;
-		extern const Color white;
-		extern const Color white_smoke;
-		extern const Color yellow;
-		extern const Color yellow_green;
-	}
 
 	void init();
 	std::string fg(uint8_t r, uint8_t g, uint8_t b);
-	std::string fg(const Color& color);
+	std::string fg(const color& c);
 	std::string bg(uint8_t r, uint8_t g, uint8_t b);
-	std::string bg(const Color& color);
+	std::string bg(const color& c);
 	void clear();
 	extern const std::string reset;
 	extern const std::string bold;
--- src/console.cpp
+++ src/console.cpp
@@ -6,148 +6,6 @@
 	#include <windows.h>
 #endif
 
-const console::Color console::color::alice_blue = console::Color{240, 248, 255};
-const console::Color console::color::antique_white = console::Color{250, 235, 215};
-const console::Color console::color::aqua = console::Color{0, 255, 255};
-const console::Color console::color::aquamarine = console::Color{127, 255, 212};
-const console::Color console::color::azure = console::Color{240, 255, 255};
-const console::Color console::color::beige = console::Color{245, 245, 220};
-const console::Color console::color::bisque = console::Color{255, 228, 196};
-const console::Color console::color::black = console::Color{0, 0, 0};
-const console::Color console::color::blanched_almond = console::Color{255, 235, 205};
-const console::Color console::color::blue = console::Color{0, 0, 255};
-const console::Color console::color::blue_violet = console::Color{138, 43, 226};
-const console::Color console::color::brown = console::Color{165, 42, 42};
-const console::Color console::color::burly_wood = console::Color{222, 184, 135};
-const console::Color console::color::cadet_blue = console::Color{95, 158, 160};
-const console::Color console::color::chartreuse = console::Color{127, 255, 0};
-const console::Color console::color::chocolate = console::Color{210, 105, 30};
-const console::Color console::color::coral = console::Color{255, 127, 80};
-const console::Color console::color::cornflower_blue = console::Color{100, 149, 237};
-const console::Color console::color::cornsilk = console::Color{255, 248, 220};
-const console::Color console::color::crimson = console::Color{220, 20, 60};
-const console::Color console::color::cyan = console::Color{0, 255, 255};
-const console::Color console::color::dark_blue = console::Color{0, 0, 139};
-const console::Color console::color::dark_cyan = console::Color{0, 139, 139};
-const console::Color console::color::dark_golden_rod = console::Color{184, 134, 11};
-const console::Color console::color::dark_gray = console::Color{169, 169, 169};
-const console::Color console::color::dark_green = console::Color{0, 100, 0};
-const console::Color console::color::dark_khaki = console::Color{189, 183, 107};
-const console::Color console::color::dark_magenta = console::Color{139, 0, 139};
-const console::Color console::color::dark_olive_green = console::Color{85, 107, 47};
-const console::Color console::color::dark_orange = console::Color{255, 140, 0};
-const console::Color console::color::dark_orchid = console::Color{153, 50, 204};
-const console::Color console::color::dark_red = console::Color{139, 0, 0};
-const console::Color console::color::dark_salmon = console::Color{233, 150, 122};
-const console::Color console::color::dark_sea_green = console::Color{143, 188, 143};
-const console::Color console::color::dark_slate_blue = console::Color{72, 61, 139};
-const console::Color console::color::dark_slate_gray = console::Color{47, 79, 79};
-const console::Color console::color::dark_turquoise = console::Color{0, 206, 209};
-const console::Color console::color::dark_violet = console::Color{148, 0, 211};
-const console::Color console::color::deep_pink = console::Color{255, 20, 147};
-const console::Color console::color::deep_sky_blue = console::Color{0, 191, 255};
-const console::Color console::color::dim_gray = console::Color{105, 105, 105};
-const console::Color console::color::dodger_blue = console::Color{30, 144, 255};
-const console::Color console::color::fire_brick = console::Color{178, 34, 34};
-const console::Color console::color::floral_white = console::Color{255, 250, 240};
-const console::Color console::color::forest_green = console::Color{34, 139, 34};
-const console::Color console::color::fuchsia = console::Color{255, 0, 255};
-const console::Color console::color::gainsboro = console::Color{220, 220, 220};
-const console::Color console::color::ghost_white = console::Color{248, 248, 255};
-const console::Color console::color::gold = console::Color{255, 215, 0};
-const console::Color console::color::golden_rod = console::Color{218, 165, 32};
-const console::Color console::color::gray = console::Color{128, 128, 128};
-const console::Color console::color::green = console::Color{0, 128, 0};
-const console::Color console::color::green_yellow = console::Color{173, 255, 47};
-const console::Color console::color::honey_dew = console::Color{240, 255, 240};
-const console::Color console::color::hot_pink = console::Color{255, 105, 180};
-const console::Color console::color::indian_red = console::Color{205, 92, 92};
-const console::Color console::color::indigo = console::Color{75, 0, 130};
-const console::Color console::color::ivory = console::Color{255, 255, 240};
-const console::Color console::color::khaki = console::Color{240, 230, 140};
-const console::Color console::color::lavender = console::Color{230, 230, 250};
-const console::Color console::color::lavender_blush = console::Color{255, 240, 245};
-const console::Color console::color::lawn_green = console::Color{124, 252, 0};
-const console::Color console::color::lemon_chiffon = console::Color{255, 250, 205};
-const console::Color console::color::light_blue = console::Color{173, 216, 230};
-const console::Color console::color::light_coral = console::Color{240, 128, 128};
-const console::Color console::color::light_cyan = console::Color{224, 255, 255};
-const console::Color console::color::light_golden_rod_yellow = console::Color{250, 250, 210};
-const console::Color console::color::light_gray = console::Color{211, 211, 211};
-const console::Color console::color::light_green = console::Color{144, 238, 144};
-const console::Color console::color::light_pink = console::Color{255, 182, 193};
-const console::Color console::color::light_salmon = console::Color{255, 160, 122};
-const console::Color console::color::light_sea_green = console::Color{32, 178, 170};
-const console::Color console::color::light_sky_blue = console::Color{135, 206, 250};
-const console::Color console::color::light_slate_gray = console::Color{119, 136, 153};
-const console::Color console::color::light_steel_blue = console::Color{176, 196, 222};
-const console::Color console::color::light_yellow = console::Color{255, 255, 224};
-const console::Color console::color::lime = console::Color{0, 255, 0};
-const console::Color console::color::lime_green = console::Color{50, 205, 50};
-const console::Color console::color::linen = console::Color{250, 240, 230};
-const console::Color console::color::magenta = console::Color{255, 0, 255};
-const console::Color console::color::maroon = console::Color{128, 0, 0};
-const console::Color console::color::medium_aquamarine = console::Color{102, 205, 170};
-const console::Color console::color::medium_blue = console::Color{0, 0, 205};
-const console::Color console::color::medium_orchid = console::Color{186, 85, 211};
-const console::Color console::color::medium_purple = console::Color{147, 112, 219};
-const console::Color console::color::medium_sea_green = console::Color{60, 179, 113};
-const console::Color console::color::medium_slate_blue = console::Color{123, 104, 238};
-const console::Color console::color::medium_spring_green = console::Color{0, 250, 154};
-const console::Color console::color::medium_turquoise = console::Color{72, 209, 204};
-const console::Color console::color::medium_violet_red = console::Color{199, 21, 133};
-const console::Color console::color::midnight_blue = console::Color{25, 25, 112};
-const console::Color console::color::mint_cream = console::Color{245, 255, 250};
-const console::Color console::color::misty_rose = console::Color{255, 228, 225};
-const console::Color console::color::moccasin = console::Color{255, 228, 181};
-const console::Color console::color::navajo_white = console::Color{255, 222, 173};
-const console::Color console::color::navy = console::Color{0, 0, 128};
-const console::Color console::color::old_lace = console::Color{253, 245, 230};
-const console::Color console::color::olive = console::Color{128, 128, 0};
-const console::Color console::color::olive_drab = console::Color{107, 142, 35};
-const console::Color console::color::orange = console::Color{255, 165, 0};
-const console::Color console::color::orange_red = console::Color{255, 69, 0};
-const console::Color console::color::orchid = console::Color{218, 112, 214};
-const console::Color console::color::pale_golden_rod = console::Color{238, 232, 170};
-const console::Color console::color::pale_green = console::Color{152, 251, 152};
-const console::Color console::color::pale_turquoise = console::Color{175, 238, 238};
-const console::Color console::color::pale_violet_red = console::Color{219, 112, 147};
-const console::Color console::color::papaya_whip = console::Color{255, 239, 213};
-const console::Color console::color::peach_puff = console::Color{255, 218, 185};
-const console::Color console::color::peru = console::Color{205, 133, 63};
-const console::Color console::color::pink = console::Color{255, 192, 203};
-const console::Color console::color::plum = console::Color{221, 160, 221};
-const console::Color console::color::powder_blue = console::Color{176, 224, 230};
-const console::Color console::color::purple = console::Color{128, 0, 128};
-const console::Color console::color::rebecca_purple = console::Color{102, 51, 153};
-const console::Color console::color::red = console::Color{255, 0, 0};
-const console::Color console::color::rosy_brown = console::Color{188, 143, 143};
-const console::Color console::color::royal_blue = console::Color{65, 105, 225};
-const console::Color console::color::saddle_brown = console::Color{139, 69, 19};
-const console::Color console::color::salmon = console::Color{250, 128, 114};
-const console::Color console::color::sandy_brown = console::Color{244, 164, 96};
-const console::Color console::color::sea_green = console::Color{46, 139, 87};
-const console::Color console::color::sea_shell = console::Color{255, 245, 238};
-const console::Color console::color::sienna = console::Color{160, 82, 45};
-const console::Color console::color::silver = console::Color{192, 192, 192};
-const console::Color console::color::sky_blue = console::Color{135, 206, 235};
-const console::Color console::color::slate_blue = console::Color{106, 90, 205};
-const console::Color console::color::slate_gray = console::Color{112, 128, 144};
-const console::Color console::color::snow = console::Color{255, 250, 250};
-const console::Color console::color::spring_green = console::Color{0, 255, 127};
-const console::Color console::color::steel_blue = console::Color{70, 130, 180};
-const console::Color console::color::tan = console::Color{210, 180, 140};
-const console::Color console::color::teal = console::Color{0, 128, 128};
-const console::Color console::color::thistle = console::Color{216, 191, 216};
-const console::Color console::color::tomato = console::Color{255, 99, 71};
-const console::Color console::color::turquoise = console::Color{64, 224, 208};
-const console::Color console::color::violet = console::Color{238, 130, 238};
-const console::Color console::color::wheat = console::Color{245, 222, 179};
-const console::Color console::color::white = console::Color{255, 255, 255};
-const console::Color console::color::white_smoke = console::Color{245, 245, 245};
-const console::Color console::color::yellow = console::Color{255, 255, 0};
-const console::Color console::color::yellow_green = console::Color{154, 205, 50};
-
 const std::string console::reset = "\x1b[0m";
 const std::string console::bold = "\x1b[1m";
 const std::string console::faint = "\x1b[2m";
@@ -178,9 +36,12 @@ std::string console::fg(uint8_t r, uint8_t g, uint8_t b)
 	return std::format("\x1b[38;2;{:d};{:d};{:d}m", r, g, b);
 }
 
-std::string console::fg(const Color& color)
+std::string console::fg(const color& c)
 {
-	return std::format("\x1b[38;2;{:d};{:d};{:d}m", color.r, color.g, color.b);
+	uint8_t r = uint32_t(c) >> 16;
+	uint8_t g = uint32_t(c) >> 8;
+	uint8_t b = uint32_t(c);
+	return console::fg(r, g, b);
 }
 
 std::string console::bg(uint8_t r, uint8_t g, uint8_t b)
@@ -188,9 +49,12 @@ std::string console::bg(uint8_t r, uint8_t g, uint8_t b)
 	return std::format("\x1b[48;2;{:d};{:d};{:d}m", r, g, b);
 }
 
-std::string console::bg(const Color& color)
+std::string console::bg(const color& c)
 {
-	return std::format("\x1b[48;2;{:d};{:d};{:d}m", color.r, color.g, color.b);
+	uint8_t r = uint32_t(c) >> 16;
+	uint8_t g = uint32_t(c) >> 8;
+	uint8_t b = uint32_t(c);
+	return console::bg(r, g, b);
 }
 
 void console::clear()
